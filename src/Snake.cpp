9ea5cc74e77b733ad7407641758351425b16446a
Aymen EL OUAGOUTI <aymen.elouagouti@gmail.com>
2023-09-13 21:28:51 +0200

    Add Snake header and cmake

---
+++ src/Snake.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Aymen on 13/09/2023.
+//
+
+#include "Snake.h"
+
+int Snake::getLength() const {
+    return length;
+}
+
+void Snake::setLength(int length) {
+    Snake::length = length;
+}
---
+++ src/Snake.h
@@ -0,0 +1,23 @@
+//
+// Created by Aymen on 13/09/2023.
+//
+
+#ifndef RUN_FROM_SNAKE_SNAKE_H
+#define RUN_FROM_SNAKE_SNAKE_H
+
+
+
+class Snake {
+private:
+    int length;
+
+public:
+    Snake();
+
+
+    int getLength() const;
+    void setLength(const int & length);
+};
+
+
+#endif //RUN_FROM_SNAKE_SNAKE_H
