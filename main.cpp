fa3430209109e7934c884d4ebb053d2bb10d2620
Aymen EL OUAGOUTI <aymen.elouagouti@gmail.com>
2023-10-12 16:13:17 +0200

    Update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "src/Person.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    auto *p = new Person("Aymen");
+
+    cout << "\nThe person's name: " << p->getName();
+    cout << "\nThe person's HP: " << p->getHp();
+
     return 0;
 }
